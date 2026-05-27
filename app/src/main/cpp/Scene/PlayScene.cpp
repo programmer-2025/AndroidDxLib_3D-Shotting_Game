be16b447df8a2b5271ea8fb2d1f8c983a73f5b12
programming-2025 <snowsansub011307@gmail.com>
2026-05-27 22:30:09 +0900

    ＜追加＞ プレイヤーを召喚できるようになりました

--- app/src/main/cpp/Scene/PlayScene.cpp
+++ app/src/main/cpp/Scene/PlayScene.cpp
@@ -4,12 +4,15 @@
 
 #include "PlayScene.h"
 #include "../Object/Enemy.h"
+#include "../Object/Player.h"
 
 namespace {
+    Player* player = nullptr;
 }
 
 void PlayScene::Init() {
     new Enemy(Location3D{0, 0, 0}, Vector3D{0, 0, 0});
+    player = new Player(Location3D{0, 0, 0}, Vector3D{0, 0, 0});
 }
 
 void PlayScene::Update() {
