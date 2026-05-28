e0362b6fde6c3298ccf1c718f17df695db863de4
programming-2025 <snowsansub011307@gmail.com>
2026-05-28 21:16:29 +0900

    ＜追加＞　ライトを追加しました

--- app/src/main/cpp/Scene/PlayScene.cpp
+++ app/src/main/cpp/Scene/PlayScene.cpp
@@ -2,6 +2,7 @@
 // Created by user on 2026/05/25.
 //
 
+#include <DxLib.h>
 #include "PlayScene.h"
 #include "../Object/Enemy.h"
 #include "../Object/Player.h"
@@ -11,6 +12,7 @@ namespace {
 }
 
 void PlayScene::Init() {
+    ChangeLightTypeDir(VECTOR{0, 1, 0});
     new Enemy(Location3D{0, 0, 0}, Vector3D{0, 0, 0});
     player = new Player(Location3D{0, 0, 0}, Vector3D{0, 0, 0});
 }
